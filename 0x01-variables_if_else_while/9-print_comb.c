d0f22d5c54c3da346b3184fe403e285b2754d12c
StacyKioko <stacy.mueke22@gmail.com>
2022-06-03 11:54:20 -0700

    Printing possible combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		putchar(c);
-	       
-          	if (c != '9')
+		putchar
+
+			if (c != '9')
 		{
 			putchar(',');
 		        putchar(' ');
