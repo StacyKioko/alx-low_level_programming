eded61bdcc7151efbf5ddcc0c131935dc051429e
StacyKioko <stacy.mueke22@gmail.com>
2022-06-04 07:05:00 -0700

    UNIX simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
@@ -8,6 +7,7 @@ int main(void)
 {
         write(2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      58);
+	      59);
+
 	return (1);
 }
