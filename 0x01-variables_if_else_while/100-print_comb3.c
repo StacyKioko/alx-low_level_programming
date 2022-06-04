513075696baeca0d5390a1ad3f828a885c5615d0
StacyKioko <stacy.mueke22@gmail.com>
2022-06-04 05:51:12 -0700

    Two digit combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints combination of numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+	        if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+		putchar('\n');
+
+		return (0);
+}
