7c2e99bea85ce9635883a25aaf4eb4b81c081881
StacyKioko <stacy.mueke22@gmail.com>
2022-06-08 07:16:58 -0700

    Printing times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,6 +27,14 @@ void print_times_table(int n)
 				}
 				else if (z > 9)
 				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
 					if (y != 0)
 					{
 						_putchar(',');
