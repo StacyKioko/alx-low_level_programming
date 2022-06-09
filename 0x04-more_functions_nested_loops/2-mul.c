a4e50a048871dd4b40334b1ad353d2e87dfca7ed
StacyKioko <stacy.mueke22@gmail.com>
2022-06-09 09:21:42 -0700

    Multiplying two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,7 +5,7 @@
  * @x: The first number to multiply
  * @y: The second number to multiply with first number
  *
- * Return: The result of multiplication of x param and y
+ * Return: The result of multiplication x and y
  */
 int mul(int x, int y)
 {
