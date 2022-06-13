db209279c875c7e0bad06adaca16d709c408f759
StacyKioko <stacy.mueke22@gmail.com>
2022-06-13 08:30:12 -0700

    Pointer to int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * updates the value it points to to 98
+ * @n: Parameter to be tested
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
