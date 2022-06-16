127be0c3321bb1992e24d80063bc1309cc97a367
StacyKioko <stacy.mueke22@gmail.com>
2022-06-16 07:24:50 -0700

    Printing code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * write your line of code here...
+	 * main - code to check
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
