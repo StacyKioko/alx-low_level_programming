1f98e17c3d5657fa864783159ee474e397197c17
StacyKioko <stacy.mueke22@gmail.com>
2022-06-22 10:32:01 -0700

    Natural number squareroot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: int number
+ * Return: If no natural square root, return -1. Else return natural
+ * square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (halp(n, 1));
+}
+
+/**
+ * _sqrt - Calculates natural square root
+ * @n: number to determine if square root
+ * @i: iterate number
+ *
+ * Return: the natural square root
+ */
+int _sqrt(int n, int i)
+{
+	int sqrt = i * i;
+
+	if (sqrt > n)
+		return (-1);
+
+	if (sqrt == n)
+		return (i);
+
+	return (_sqrt(n, i + 1));
+}
