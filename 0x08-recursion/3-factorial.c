ccf2a58733e62129a7a83a8ff33eca36ecc9d2ae
StacyKioko <stacy.mueke22@gmail.com>
2022-06-22 10:10:16 -0700

    Factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,8 +9,12 @@ int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n < 2)
-		return (n);
-	else
-		return (n * factorial(n - 1));
+	{
+		else if (n < 2)
+			return (n);
+	}
+	{
+		else
+			return (n * factorial(n - 1));
+	}
 }
