072499f5ef864a4c6497431c188b2bb27c6d8b23
StacyKioko <stacy.mueke22@gmail.com>
2022-07-11 08:12:42 -0700

    Printing name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,7 +7,8 @@
  *
  * Return: Nothing
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
