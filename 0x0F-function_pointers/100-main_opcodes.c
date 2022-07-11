d06910ee536a5b67997e9faa565c9697a3658fe6
StacyKioko <stacy.mueke22@gmail.com>
2022-07-11 08:57:27 -0700

    Printing opcodes of the main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
 
 /**
- * main - ...
- * @argc: ...
- * @argv: ...
+ * main - function with two arguments
+ * @argc: int type argument count
+ * @argv: char type argument array
  *
- * Return: ...
+ * Description: print opcode
+ * Return: nothing
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
+	int count;
 
-	if (argc == 2)
+	count = 0;
+	if (argc != 2)
 	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (count < atoi(argv[1]))
+	{
+		printf("%02x", *((unsigned char *)main + count));
+		count++;
+		if (atoi(argv[1]) > count)
 		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
+			printf(" ");
 		}
 	}
-
+	printf("\n");
 	return (0);
 }
