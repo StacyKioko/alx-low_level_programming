52254a118c192c9d851f5ee9e5d87973bbef1e50
StacyKioko <stacy.mueke22@gmail.com>
2022-07-14 21:35:45 +0300

    Adding new node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,4 +19,5 @@ typedef struct list_s
 int _strlen(const char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node_end(list_t **head, const char *str);
 #endif
