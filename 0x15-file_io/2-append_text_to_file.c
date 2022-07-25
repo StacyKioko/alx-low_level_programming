c120d2b66faef9f70a57cb506cc46616077592ae
StacyKioko <96838054+StacyKioko@users.noreply.github.com>
2022-07-25 23:32:45 +0300

    Appending text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,7 +2,7 @@
 
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: file to append the text to
+ * @filename: File to append the text to
  * @text_content: content to append into the file
  *
  * Return: 1 on success and -1 on failure
